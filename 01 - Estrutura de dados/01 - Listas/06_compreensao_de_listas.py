c32987e2c965dd21bed87a63aa630340e2737e24
Kaio Assprom <kaiobrendow@hotmail.com>
2023-08-03 11:43:12 -0300

    Comentario

--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -1,4 +1,11 @@
-# Filtrar lista
+# Filtar lista normal
+#numeros = [1, 30, 21, 2, 9, 65, 34]
+#pares = []
+#for numero in numeros:
+#    if numero % 2 ==0:
+#        pares.append(numero)
+
+# Filtrar lista compreensao
 numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]
 print(pares)
